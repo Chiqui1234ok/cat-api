010aca2fd4eae3ff4f5e1f243471c7726af132fb
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2024-07-22 11:04:37 -0500

    updated

---
+++ laravel/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
+
+class ApiController extends Controller
+{
+    
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,12 +1,25 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CatController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CatController;
 
 Route::view('/', 'api');
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/signup', [AuthController::class, 'signup']);
+// Register
+Route::post('/user/register', [AuthController::class, 'registerUser']);
+// Login
+Route::post('/user/login', [AuthController::class, 'loginUser']);
 
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function()
+{
+    Route::post('/user/profile', [UserController::class, 'profile']);
+});
+/*
+    * Secured routes
+*/
 Route::middleware('auth:sanctum')->post('/cat', [CatController::class, 'store']);
